ea927212e8501b620202f28eb21b5a1693d4cd83
Kishan <kishanbhadani83@gmail.com>
2025-12-27 11:43:21 +0530

    cursour issue

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { motion } from "framer-motion"
-import { FileDownIcon, Pointer } from "lucide-react";
+import { FileDownIcon } from "lucide-react";
 import Image from "next/image"
 import HeroImage from "@/public/assets/baner.png";
 
@@ -62,7 +62,6 @@ export default function Hero() {
           >
             <p
               rel="noopener noreferrer"
-              style={{ cursor: 'pointer' }}
               className="flex text-sm font-semibold leading-6 text-foreground apple-button pe-auto"
               onClick={async () => await fileDownload()}
             >
